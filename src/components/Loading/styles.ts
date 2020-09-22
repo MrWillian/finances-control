85976bc5b7aa4d855aba293c009b8fbd73ace5dd
MrWilllian <willian.dev10@gmail.com>
2020-09-22 10:56:01 -0300

    refactor: changes on Loading component style

--- src/components/Loading/styles.ts
+++ src/components/Loading/styles.ts
@@ -2,22 +2,27 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
+  align-items: center;
 
-  width: 75%;
-  height: 2%;
+  width: 80%;
+  height: 30%;
 `;
 
 export const ProgressBar = styled.View`
   border-radius: 50px;
   background: #FFF;
 
-  elevation: 20;
+  width: 80%;
+  height: 5%;
+  margin-top: 20px;
+  elevation: 15;
 `;
 
 export const ProgressBarLine = styled.View`
   width: 65%;
   height: 80%;
+  margin-top: 1px;
   border-radius: 50px;
   background-color: #FFD700;
 `;
@@ -26,4 +31,8 @@ export const Label = styled.Text`
   color: #FFD700;
   align-self: center;
   margin-top: 10px;
+`;
+
+export const Image = styled.Image`
+  flex: 1;
 `;
