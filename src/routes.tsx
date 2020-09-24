570f1edd0431490338506f34ef650c18d5aecec9
MrWilllian <willian.dev10@gmail.com>
2020-09-24 11:31:15 -0300

    refactor: include Main route

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,6 +6,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Splash from './screens/Splash';
 import Login from './screens/Login';
 import Register from './screens/Register';
+import Main from './screens/Main';
 
 const AppStack = createStackNavigator();
 
@@ -13,6 +14,7 @@ const Routes = () => {
   return (
     <NavigationContainer>
       <AppStack.Navigator screenOptions={{ headerShown: false }}>
+        <AppStack.Screen name="Main" component={Main} />
         <AppStack.Screen name="Register" component={Register} />
         <AppStack.Screen name="Login" component={Login} />
         <AppStack.Screen name="Splash" component={Splash} />
