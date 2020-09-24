09eccbc46ed79e49614c145b7033207882169402
MrWilllian <willian.dev10@gmail.com>
2020-09-24 08:49:29 -0300

    refactor: include BottomInfoText style

---
+++ src/components/BottomInfoText/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native';
+
+export const Text = styled.Text`
+  color: #FFF;
+  font-family: 'Glegoo';
+  font-size: 14px;
+  elevation: 20;
+  letter-spacing: 1px;
+`;
