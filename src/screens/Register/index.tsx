dcd099f4f2ebf4e792e03277fcbdd5ab4d075b06
MrWilllian <willian.dev10@gmail.com>
2020-09-25 15:13:54 -0300

    refactor: implement phone masked input and password input

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Alert } from 'react-native';
 
 import SimpleForm from '../../components/SimpleForm';
 import Input from '../../components/Input';
@@ -13,15 +14,32 @@ import { iNavigationProps } from '../../utils/iNavigationProps';
 import { Container } from './styles';
 
 const Register: React.FC<iNavigationProps> = ({ navigation }) => {
+  const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [phoneNumber, setPhoneNumber] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <Container>
       <Title>Crie sua conta...</Title>
       <SimpleForm>
-        <Input name="Nome" icon="person" />
-        <Input name="Email" icon="mail" />
-        <Input name="Telefone" icon="phone" />
-        <Input name="Senha" icon="lock" />
-        <Button name="Cadastrar" route="MainStack" navigation={navigation} />
+        <Input name="Nome" value={name} icon="person" onChangeText={name => setName(name)} />
+        <Input name="Email" value={email} icon="mail" onChangeText={email => setEmail(email)} />
+        <Input 
+          name="Telefone" 
+          value={phoneNumber} 
+          icon="call" 
+          onChangeText={phoneNumber => setPhoneNumber(phoneNumber)}
+          isPhoneNumber={true} />
+        <Input 
+          name="Senha" 
+          value={password} 
+          icon="lock-closed" 
+          onChangeText={password => setPassword(password)}
+          secureTextEntry={true} />
+        
+        {/* <Button name="Cadastrar" /> */}
+
       </SimpleForm>
       <BottomInfo>
         <BottomInfoText>
