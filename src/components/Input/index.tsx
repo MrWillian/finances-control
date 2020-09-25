12ea8bafb91184b49ba0875f5d2e5ea44aa3f72f
MrWilllian <willian.dev10@gmail.com>
2020-09-25 10:52:06 -0300

    refactor: adapt Input component to accept values

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,15 +4,17 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Container, TextInput } from './styles';
 
 interface Props {
-  name ?: string;
-  icon : string;
+  name?: string;
+  icon: string;
+  value?: string;
+  onChangeText(operation: any): any;
 }
 
-const Input: React.FC<Props> = ({ name, icon }) => {
+const Input: React.FC<Props> = ({ name, icon, value, onChangeText }) => {
   return (
     <Container>
       <Icon name={icon} size={30} color="#2D142C" />
-      <TextInput placeholder={name} />
+      <TextInput value={value} placeholder={name} onChangeText={onChangeText} />
     </Container>
   );
 }
