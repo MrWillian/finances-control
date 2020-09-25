5364489f5305e86db2c85bc1ab5066c96022b6ab
MrWilllian <willian.dev10@gmail.com>
2020-09-25 10:53:17 -0300

    refactor: include login and storage method

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Alert } from 'react-native';
 
 import SimpleForm from '../../components/SimpleForm';
 import Title from '../../components/Title';
@@ -10,16 +11,38 @@ import BottomInfoLink from '../../components/BottomInfoLink';
 
 import { iNavigationProps } from '../../utils/iNavigationProps';
 
+import AuthController from '../../controllers/AuthController';
+import StorageController from '../../controllers/StorageController';
+
 import { Container } from './styles';
 
 const Login: React.FC<iNavigationProps> = ({ navigation }) => {
+	const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  let authController = new AuthController();
+  let storageController = new StorageController();
+
+  async function handleLogin() { 
+    await (authController.login(email, password)).then(user => {
+      if (user.data == undefined) {
+        Alert.alert('Erro', 'Ocorreu um erro ao tentar fazer login, tente novamente!', [
+          { style: "cancel" }
+        ]);
+        return;
+      }
+      storageController.setItem('@finances/user', user.data);
+      navigation.navigate('MainStack');
+    });
+  }
+
   return (
     <Container>
       <Title>Faça login com seu email e senha...</Title>
       <SimpleForm>
-        <Input name="Email" icon="mail" />
-        <Input name="Senha" icon="lock" />
-        <Button name="Entrar" route="MainStack" navigation={navigation} />
+        <Input name="Email" value={email} icon="mail" onChangeText={email => setEmail(email)} />
+        <Input name="Senha" value={password} icon="lock" onChangeText={password => setPassword(password)} />
+        <Button name="Entrar" onPress={handleLogin} />
       </SimpleForm>
       <BottomInfo>
         <BottomInfoText>Não tem uma conta?</BottomInfoText>
