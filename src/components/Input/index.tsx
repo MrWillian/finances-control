4a35e3232d4e8d68c53e2e4860096c728520f8f8
MrWilllian <willian.dev10@gmail.com>
2020-09-25 14:35:41 -0300

    refactor: include Show/Hide password feature

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,18 +1,36 @@
-import React from 'react';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import React, { useState } from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import { Container, TextInput } from './styles';
 
 interface Props {
-  name ?: string;
-  icon : string;
+  name?: string;
+  icon: string;
+  secureTextEntry?: boolean;
 }
 
-const Input: React.FC<Props> = ({ name, icon }) => {
+const Input: React.FC<Props> = ({ name, icon, secureTextEntry }) => {
+  const [showPassword, setShowPassword] = useState(secureTextEntry);
+
+  const changeShowPasswordIcon = () => {
+    setShowPassword(!showPassword);
+  }
+
   return (
     <Container>
       <Icon name={icon} size={30} color="#2D142C" />
-      <TextInput placeholder={name} />
+      <TextInput 
+        placeholder={name} 
+        secureTextEntry={showPassword} />
+      {!secureTextEntry ? 
+        <></>
+        : <Icon 
+            style={{marginRight: 'auto'}}
+            name={showPassword ? 'eye' : 'eye-off'} 
+            size={30} 
+            color="#2D142C"
+            onPress={changeShowPasswordIcon} />
+      }
     </Container>
   );
 }
