d64c95bdc39c064ed0a47fbc753172994426605e
MrWilllian <willian.dev10@gmail.com>
2020-09-30 15:00:36 -0300

    refactor: include CustomInputs export - refactorying Inputs

---
+++ src/components/CustomInputs/index.tsx
@@ -0,0 +1,11 @@
+import TextInput from './TextInput';
+import PasswordInput from './PasswordInput';
+import PhoneInput from './PhoneInput';
+import MoneyInput from './MoneyInput';
+
+export {
+  TextInput, 
+  PasswordInput, 
+  PhoneInput, 
+  MoneyInput
+};
