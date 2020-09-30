52126fb05c6fd93a370b848875f59c5196874335
MrWilllian <willian.dev10@gmail.com>
2020-09-30 15:35:06 -0300

    refactor: refactorying Form components - moved files

---
+++ src/components/FormBasicComponents/Title/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Text } from './styles';
+
+const Title: React.FC = ({ children }) => {
+  return (
+    <Text>
+      {children}
+    </Text>
+  );
+}
+
+export default Title;
---
+++ src/components/FormBasicComponents/Title/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native';
+
+export const Text = styled.Text`
+  color: #FFF;
+  font-family: 'Glegoo';
+  font-size: 18px;
+  elevation: 20;
+`;
