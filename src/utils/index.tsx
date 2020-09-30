3db0fa40bd3f8b70e7efe311fc62d4122baeac93
MrWilllian <willian.dev10@gmail.com>
2020-09-30 15:37:58 -0300

    refactor: refactorying utils add export file

---
+++ src/utils/index.tsx
@@ -0,0 +1,9 @@
+import { iNavigationProps } from './iNavigationProps';
+import { iCustomInputProps } from './iCustomInputProps';
+import { FieldType } from './FieldType';
+
+export {
+  iNavigationProps,
+  iCustomInputProps,
+  FieldType
+}
