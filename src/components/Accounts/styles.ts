2153326f70b9decf28176a38e801103d0763c31c
MrWilllian <willian.dev10@gmail.com>
2020-09-30 11:04:11 -0300

    refactor: adapts on Account style

--- src/components/Accounts/styles.ts
+++ src/components/Accounts/styles.ts
@@ -1,18 +1,21 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.ScrollView`
-  padding: 30px;
-  width: 100%;
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
 `;
 
 export const Title = styled.Text`
   font-family: 'Glegoo';
   font-size: 18px;
   color: #C8C8C8;
+  margin-bottom: 10px;
 `;
 
 export const AccountsContainer = styled.View`
-  margin: 10px 0;
+  flex: 1;
+  margin: 5px 0;
+  padding: 30px;
 `;
 
 export const PlusButtonContainer = styled.TouchableOpacity`
@@ -31,9 +34,11 @@ export const PlusButtonContainer = styled.TouchableOpacity`
 `;
 
 export const BillsContainer = styled.View`
+  flex: 1;
   display: flex;
   justify-content: center;
   margin: 10px 0;
+  padding: 30px;
 `;
 
 export const BillsTitle = styled.Text`
@@ -43,5 +48,5 @@ export const BillsTitle = styled.Text`
 `;
 
 export const BillsScroll = styled.ScrollView`
-  margin: 10px 0;
+  margin: 20px 0;
 `;
