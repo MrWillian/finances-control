b47bb429b206f3df592a47b571bc8705104bc209
MrWilllian <willian.dev10@gmail.com>
2020-10-08 15:46:32 -0300

    refactor: include remove item from storage

---
+++ src/controllers/storage/StorageController.ts
@@ -0,0 +1,30 @@
+import storage from '../../services/storage';
+
+export default class StorageController {
+  async setItem(key: string, value: any) {
+    try {
+      const data = JSON.stringify(value);
+      await storage.save({ key, data });
+      return true;
+    } catch(error) {
+      return error;
+    }
+  };
+
+  async getItem(key: string) {
+    try {
+      const jsonValue = await storage.load({ key });
+      return jsonValue != null ? JSON.parse(jsonValue) : null;
+    } catch(error) {
+      return error;
+    }
+  };
+
+  async removeItem(key: string) {
+    try {
+      return await storage.remove({ key });
+    } catch(error) {
+      return error;
+    }
+  }
+}
