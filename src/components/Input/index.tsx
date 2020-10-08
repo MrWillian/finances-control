c256403d46697f9d49d71af811956a1127ce12b1
MrWilllian <willian.dev10@gmail.com>
2020-10-08 15:47:30 -0300

    refactor: implement news TextInput props

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -7,7 +7,8 @@ import { iCustomInputProps, FieldType } from '../../utils';
 import { Container } from './styles';
 
 const Input: React.FC<iCustomInputProps> = ({ 
-  name, icon, value, onChangeText, focus, type, includeRawValueInChangeText
+  name, icon, value, onChangeText, focus, type, includeRawValueInChangeText, autoCapitalize,
+  textContentType, keyboardType, autoCorrect, autoCompleteType
 }) => {
   const secureTextEntry = type === FieldType.PASSWORD;
   const [showPassword, setShowPassword] = useState(secureTextEntry);
@@ -17,15 +18,20 @@ const Input: React.FC<iCustomInputProps> = ({
   const getInputForType = () => {
     switch(type) {
       case FieldType.TEXT:
-        return <TextInput name={name} value={value} onChangeText={onChangeText} focus={focus} />;
+        return <TextInput name={name} value={value} onChangeText={onChangeText} focus={focus} 
+                autoCapitalize={autoCapitalize} textContentType={textContentType} keyboardType={keyboardType}
+                autoCorrect={autoCorrect} autoCompleteType={autoCompleteType} />;
+
       case FieldType.PASSWORD:
         return <PasswordInput name={name} value={value} onChangeText={onChangeText} focus={focus}
-                showPassword={showPassword} />
+                showPassword={showPassword} autoCapitalize={autoCapitalize} />;
+
       case FieldType.PHONENUMBER:
         return <PhoneInput name={name} value={value} onChangeText={onChangeText} focus={focus} />;
+
       case FieldType.MONEY:
         return <MoneyInput name={name} value={value} onChangeText={onChangeText} focus={focus} 
-            includeRawValueInChangeText={includeRawValueInChangeText} />;
+                includeRawValueInChangeText={includeRawValueInChangeText} />;
       default: 
         return;
     }
