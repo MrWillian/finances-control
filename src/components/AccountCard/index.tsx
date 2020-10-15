e37cac565f1903e44facc1ab7f796526751e6e1f
MrWilllian <willian.dev10@gmail.com>
2020-10-15 15:19:44 -0300

    refactor: change style

--- src/components/AccountCard/index.tsx
+++ src/components/AccountCard/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import LinearGradient from 'react-native-linear-gradient';
 
-import { Container, Title, Value } from './styles';
+import { Container, AccountInfo, Title, Value, See, SeeLabel, Divisor } from './styles';
 
 interface Props {
   title : string;
@@ -9,27 +9,38 @@ interface Props {
 }
 
 const AccountCard: React.FC<Props> = ({ title, value }) => {
-  return (
-    <LinearGradient 
-      colors={[
-        'rgba(128, 19, 54, 1)', 
-        'rgba(128, 19, 54, 0.7)', 
-        'rgba(128, 19, 54, 0.5)', 
-        'rgba(128, 19, 54, 0.3)',
-      ]}
-      style={{
-        flex: 1, 
-        borderRadius: 5, 
-        padding: 20, 
-        marginTop: 10
-      }}>
-      
-      <Container>
-        <Title>{title}</Title>
-        <Value>{value}</Value>
-      </Container>
 
-    </LinearGradient>
+  const handleSeeAccount = () => {
+    
+  }
+
+  return (
+    // <LinearGradient 
+    //   colors={[
+    //     'rgba(128, 19, 54, 1)', 
+    //     'rgba(128, 19, 54, 0.7)', 
+    //     'rgba(128, 19, 54, 0.5)', 
+    //     'rgba(128, 19, 54, 0.3)',
+    //   ]}
+    //   style={{
+    //     flex: 1, 
+    //     borderRadius: 5, 
+    //     padding: 20, 
+    //     marginTop: 10
+    //   }}>
+      <>
+        <Container>
+          <AccountInfo>
+            <Title>{title}</Title>
+            <Value>{value}</Value>
+          </AccountInfo>
+          <See onPress={handleSeeAccount}>
+            <SeeLabel>Visualizar</SeeLabel>
+          </See>
+        </Container>
+        <Divisor />
+      </>
+    // </LinearGradient>
   );
 }
 
