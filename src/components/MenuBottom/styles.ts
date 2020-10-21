faa3d5d6f2b35329dd772bc96ad31322e8ed6f75
MrWilllian <willian.dev10@gmail.com>
2020-10-21 10:16:48 -0300

    style: changes on design of MenuBottom

--- src/components/MenuBottom/styles.ts
+++ src/components/MenuBottom/styles.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components/native';
 
+interface MenuButtonProps {
+  isActive?: boolean;
+}
+
 export const Container = styled.View`
   position: absolute;
   flex: 0.1;
@@ -8,7 +12,7 @@ export const Container = styled.View`
   bottom: 0;
   height: 10%;
   width: 100%;
-  background-color: rgba(0, 0, 0, 0.0);
+  /* background-color: rgba(27, 37, 64, 0.8); */
 
   display: flex;
   flex-direction: row;
@@ -16,22 +20,27 @@ export const Container = styled.View`
   justify-content: space-around;
 `;
 
-export const MenuButton = styled.TouchableWithoutFeedback`
+export const MenuButton = styled.TouchableOpacity`
   flex: 1;
   display: flex;
   align-items: center;
   justify-content: center;
 `;
 
-export const MenuButtonContainer = styled.View`
+export const MenuButtonContainer = styled.View<MenuButtonProps>`
   display: flex;
+  flex: 1;
   align-items: center;
   justify-content: center;
 
+  top: ${props => props.isActive ? -5 : 0}px;
+  border-bottom-width: ${props => props.isActive ? 2 : 0}px;
+  border-bottom-color: #6269F1;
 `;
 
 export const MenuButtonLabel = styled.Text`
   color: #91A0D0;
-  font-size: 10px;
-  font-family: 'Comfortaa-Medium';
+  font-size: 11px;
+  font-family: 'Comfortaa-Bold';
+  elevation: 5;
 `;
