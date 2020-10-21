85beffb5449395aada45a8775dbc3f1e938eb90d
MrWilllian <willian.dev10@gmail.com>
2020-10-21 10:19:15 -0300

    refactor: include ExpensesList index

---
+++ src/components/ExpensesList/index.tsx
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import { FlatList } from 'react-native';
+import ExpensesItem, { Expense } from '../ExpensesItem';
+
+export interface Props {
+  expensesList: Expense[];
+}
+
+const ExpensesList: React.FC<Props> = ({expensesList}) => {
+  const [expenses, setExpenses] = useState<Expense[]>(expensesList);
+
+  const keyExtractor = (item: Expense) => item.id;
+
+  const renderItem = ({ item }: {item: Expense}) => (
+    <ExpensesItem 
+      id={item.id} 
+      title={item.title} 
+      description={item.description} 
+      value={item.value} 
+    />
+  )
+
+  return (
+    <FlatList<Expense>
+      data={expenses}
+      renderItem={renderItem}
+      keyExtractor={keyExtractor} 
+      style={{ flex: 1, width: '100%', paddingLeft: 20, paddingRight: 20}}
+    />
+  );
+}
+
+export default ExpensesList;
