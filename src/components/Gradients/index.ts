7365d31ab103947afdb2038e3126e24bc2851fbc
MrWilllian <willian.dev10@gmail.com>
2020-10-21 10:53:30 -0300

    refactor: export DrawerGradient

--- src/components/Gradients/index.ts
+++ src/components/Gradients/index.ts
@@ -1,7 +1,9 @@
 import MenuBottomGradient from './MenuBottomGradient';
 import BackgroundGradient from './BackgroundGradient';
+import DrawerGradient from './DrawerGradient';
 
 export {
   MenuBottomGradient,
-  BackgroundGradient
+  BackgroundGradient,
+  DrawerGradient
 };
