9be5928649f78075f94a8e23a4396afad13c7863
MrWilllian <willian.dev10@gmail.com>
2020-10-22 11:06:24 -0300

    refactor: implement account creating

--- src/components/SheetForm/index.tsx
+++ src/components/SheetForm/index.tsx
@@ -1,14 +1,47 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Alert } from 'react-native';
 import { Button } from '../FormBasicComponents/';
 import Input from '../Input';
 import { FieldType } from '../../utils';
 
+import { AccountController, StorageController } from '../../controllers';
+
 import { Container } from './styles';
+import { useNavigation } from '@react-navigation/native';
 
 const SheetForm: React.FC = () => {
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
-  const [value, setValue] = useState('');
+  const [amount, setAmount] = useState('');
+  const [token, setToken] = useState('');
+  const navigation = useNavigation();
+
+  let accountController: AccountController = new AccountController(token);
+  let storageController = new StorageController();
+
+  useEffect(() => {  
+    getTokenStorage();
+    
+    if (token !== '') 
+    accountController = new AccountController(token);
+  }, []);
+  
+  const getTokenStorage: any = async () => setToken((await storageController.getItem('@finances/user'))['access_token']);
+
+  async function handleCreate() {
+    await (accountController.create(name, description, +amount, token)).then(account => {
+      if (account.data == undefined) {
+        Alert.alert('Erro', 'Ocorreu um erro ao tentar registrar a conta, tente novamente!', [
+          { style: "cancel" }
+        ]);
+        return;
+      }
+      navigation.navigate('Main');
+      console.log('data', account.data);
+    }).catch(error => {
+      console.log(error);
+    });
+  }
 
   return (
     <Container>
@@ -27,13 +60,15 @@ const SheetForm: React.FC = () => {
       
       <Input 
         name="Valor R$"
-        value={value}
-        onChangeText={value => setValue(value)}
+        value={amount}
+        includeRawValueInChangeText={true}
+        onChangeText={(amount, rawAmount) => setAmount(rawAmount)}
         type={FieldType.MONEY} />
 
       <Button 
         name="Registrar" 
-        onPress={() => console.log('Teste')} />
+        onPress={handleCreate} />
+
     </Container>
   );
 }
