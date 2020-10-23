ff26b7b7e7db09283a58409bb4c66469d0586f79
MrWilllian <willian.dev10@gmail.com>
2020-10-23 15:45:57 -0300

    refactor: remove AccountsContainer styled-components

--- src/components/Accounts/styles.ts
+++ src/components/Accounts/styles.ts
@@ -5,38 +5,12 @@ export const Container = styled.View`
   justify-content: center;
 `;
 
-export const Title = styled.Text`
-  font-family: 'Comfortaa-Medium';
-  font-size: 18px;
-  color: #FDFDFD;
-  margin-bottom: 10px;
-`;
-
-export const AccountsContainer = styled.View`
-  flex: 1;
-  padding: 20px 30px;
-`;
-
-export const PlusButtonContainer = styled.TouchableOpacity`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  align-self: flex-end;
-
-  background-color: #6269F1;
-
-  width: 25px;
-  height: 25px;
-  margin: 10px 0;
-  border-radius: 25px;
-  elevation: 10;
-`;
-
 export const BillsContainer = styled.View`
   flex: 1;
   display: flex;
   justify-content: center;
   padding: 10px 30px;
+  margin: 0 0 30px 0;
 `;
 
 export const BillsTitle = styled.Text`
