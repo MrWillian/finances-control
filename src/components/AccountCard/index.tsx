a8133e141875dc7fd073a319f82ad1a8b8f87959
MrWilllian <willian.dev10@gmail.com>
2020-10-27 20:28:57 -0300

    refactor: last changes on accounts delete event

--- src/components/AccountCard/index.tsx
+++ src/components/AccountCard/index.tsx
@@ -1,69 +1,80 @@
 import React, { RefObject, useRef } from 'react';
-import { PanResponder, Dimensions, UIManager, LayoutAnimation, View, PanResponderGestureState } from 'react-native';
+import { 
+  PanResponder, Dimensions, UIManager, LayoutAnimation, View, PanResponderGestureState, Alert 
+} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import { Container, DeleteContainer, Card, AccountInfo, Title, Value, See, SeeLabel } from './styles';
 
 interface Props {
-  title : string;
-  value : string;
+  id: number;
+  title: string;
+  value: string;
+  handleDelete(id: number): void;
 }
 
-const AccountCard: React.FC<Props> = ({ title, value }) => {
-  const tasks: RefObject<View> = useRef(null);
+const AccountCard: React.FC<Props> = ({ id, title, value, handleDelete }) => {
+  const cardRef: RefObject<View> = useRef(null);
+  const deleteRef: RefObject<View> = useRef(null);
+
   UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
 
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => false,
     onStartShouldSetPanResponderCapture: () => false,
-    // onMoveShouldSetResponderCapture: () => true,
     onMoveShouldSetPanResponderCapture: (event, gestureState) => {
-      const { dx, dy } = gestureState;
-      return (dx > 2 || dx < -2 || dy > 5 || dy < -5);
+      return (gestureState.dx > 2 || gestureState.dx < -2 || gestureState.dy > 5 || gestureState.dy < -5);
     },
-    onPanResponderMove: (event, gestureState) => {
-      onMoveX(gestureState.dx);
+    onPanResponderMove: (event, gestureState) => { onMoveX(gestureState.dx); },
+    onPanResponderGrant: (event, gestureState) => {
+      deleteRef.current?.setNativeProps({ x: gestureState.dx + 120 });
+      
     },
-    onPanResponderTerminate: (event, gestureState) => {
-      onPanResponder(gestureState);
-    },
-    onPanResponderRelease: (event, gestureState) => {
-      onPanResponder(gestureState);
-    },
+    onPanResponderTerminate: (event, gestureState) => { onPanResponder(gestureState); },
+    onPanResponderRelease: (event, gestureState) => { onPanResponder(gestureState); },
   });
 
   const onMoveX = (dx: number) => {
-    if (tasks.current !== null)
-      tasks.current.setNativeProps({style: { transform: [{ translateX: -80 }] }});
+    cardRef.current?.setNativeProps({style: { transform: [{ translateX: dx }] }});
+    deleteRef.current?.setNativeProps({style: { transform: [{translateX: dx+140}] }});
   }
 
   const onPanResponder = (gestureState: PanResponderGestureState) => {
-    // if (Math.abs(gestureState.dx) < Dimensions.get('window').width/2) {
-    //   console.log('onPanResponderTerminate');
-    //   if (tasks.current !== null)
-    //     tasks.current.setNativeProps({style: { transform: [{ translateX: 0 }]} });
-    // }
+    const windowWidth = Dimensions.get('window').width;
+    if (Math.abs(gestureState.dx) < windowWidth/2) {
+      cardRef.current?.setNativeProps({style: { transform: [{ translateX: 0 }]} });
+      deleteRef.current?.setNativeProps({style: {transform: [{ translateX: 0 }]} });
+    }
     
-    if (Math.abs(gestureState.dx) >= Dimensions.get('window').width/2) {
-      console.log('onPanResponderRelease');
-      console.log('Dimensions width', Dimensions.get('window').width);
+    if (Math.abs(gestureState.dx) >= windowWidth/2) {
       LayoutAnimation.configureNext(LayoutAnimation.create(300, 'easeInEaseOut', 'opacity'));
-      if (tasks.current !== null)
-        tasks.current.setNativeProps({style: { transform: [{ translateX: Dimensions.get('window').width }]} });
-      handleDelete();
+      cardRef.current?.setNativeProps({style: { transform: [{ translateX: windowWidth }]} });
+      deleteRef.current?.setNativeProps({style: {transform:[{ translateX: -(windowWidth/4) }]}});
+      handleDeleteAccount(id);
     }
   }
 
   const handleSeeAccount = () => {}
 
-  const handleDelete = () => console.log('DELETE EVENT');
+  const handleDeleteAccount = (id: number) => {
+    Alert.alert(
+      'Alerta', `Tem certeza que deseja excluir a conta '${title}'?`, [
+        { text: 'Sim', style: "default", onPress: () => handleDelete(id) },
+        { text: 'Não', style: "cancel", onPress: () => {
+          cardRef.current?.setNativeProps({style: { transform: [{ translateX: 0 }]} });
+          deleteRef.current?.setNativeProps({style: { transform: [{ translateX: 0 }]} });
+        }}
+      ]
+    );
+  };
 
   return (
     <Container>
-      <DeleteContainer>
+      <DeleteContainer ref={deleteRef}>
         <Icon name="trash-o" size={20} color="#E97A8B" />
+        {/* <Title>Deletar conta</Title> */}
       </DeleteContainer>
-      <Card ref={tasks} {...panResponder.panHandlers}>
+      <Card ref={cardRef} {...panResponder.panHandlers}>
         <AccountInfo>
           <Title>{title}</Title>
           <Value>{value}</Value>
