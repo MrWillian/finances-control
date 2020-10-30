1246886ed942328ab2a300ab95f1ad9750d5c3bb
MrWilllian <willian.dev10@gmail.com>
2020-10-30 14:34:22 -0300

    refactor: refactorin accounts actions

--- core/lib/adapters/redux/store/ducks/accounts/actions.ts
+++ core/lib/adapters/redux/store/ducks/accounts/actions.ts
@@ -1,8 +1,10 @@
 import { action } from 'typesafe-actions';
 import { AccountsTypes, Account } from './types';
 
-export const saveAccount = (data: Account) => action(AccountsTypes.SAVE_ACCOUNT, { data });
+export const loadRequest = () => action(AccountsTypes.LOAD_REQUEST);
 
-export const updateAccount = (data: Account) => action(AccountsTypes.UPDATE_ACCOUNT, { data });
+export const loadSuccess = (data: Account[]) => action(AccountsTypes.LOAD_SUCCESS, { data });
 
-export const deleteAccount = (id: number) => action(AccountsTypes.DELETE_ACCOUNT, { id });
+export const loadFailure = (data: Error) => action(AccountsTypes.LOAD_FAILURE, { data });
+
+export const deleteAccount = (id: number, token: string) => action(AccountsTypes.DELETE_ACCOUNT, { id, token });
