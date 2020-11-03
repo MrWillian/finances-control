0dffb4e1bfba108e6e19d0194c0f8c9cea6dad4e
MrWilllian <willian.dev10@gmail.com>
2020-11-03 09:05:07 -0300

    refactor: set id how undefined

--- core/lib/adapters/redux/store/ducks/accounts/types.ts
+++ core/lib/adapters/redux/store/ducks/accounts/types.ts
@@ -2,11 +2,14 @@ export enum AccountsTypes {
   LOAD_REQUEST = '@accounts/LOAD_REQUEST',
   LOAD_SUCCESS = '@accounts/LOAD_SUCCESS',
   LOAD_FAILURE = '@accounts/LOAD_FAILURE',
+  LOAD_CREATE_SUCCESS = '@accounts/LOAD_CREATE_SUCCESS',
+  LOAD_DELETE_SUCCESS = '@accounts/LOAD_DELETE_SUCCESS',
+  CREATE_ACCOUNT = '@accounts/CREATE_ACCOUNT',
   DELETE_ACCOUNT = '@accounts/DELETE_ACCOUNT',
 }
 
 export interface Account {
-  id: number;
+  id?: number;
   name: string;
   description: string;
   amount: string;
