31a159cda1b12e138dbe49b06c4b3841795cec52
MrWilllian <willian.dev10@gmail.com>
2020-11-03 09:05:29 -0300

    refactor: include createAccount action

--- core/lib/adapters/redux/store/ducks/accounts/actions.ts
+++ core/lib/adapters/redux/store/ducks/accounts/actions.ts
@@ -7,4 +7,6 @@ export const loadSuccess = (data: Account[]) => action(AccountsTypes.LOAD_SUCCES
 
 export const loadFailure = (data: Error) => action(AccountsTypes.LOAD_FAILURE, { data });
 
+export const createAccount = (data: Account, token: string) => action(AccountsTypes.CREATE_ACCOUNT, { data, token });
+
 export const deleteAccount = (id: number, token: string) => action(AccountsTypes.DELETE_ACCOUNT, { id, token });
