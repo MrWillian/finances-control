d6545ef06ca0ca8ddc240267e860ffd8e88b35ba
MrWilllian <willian.dev10@gmail.com>
2020-11-03 09:08:17 -0300

    style: include FlashMessage component style

---
+++ src/components/FlashMessage/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  position: absolute; 
+  background-color: green; 
+  width: 100%; 
+  justify-content: center; 
+  align-items: center;           
+  height: 40px;
+  bottom: -30px;
+`;
+
+export const Message = styled.Text`
+  color: #FFF;
+  font-family: 'Comfortaa-Medium';
+`;
