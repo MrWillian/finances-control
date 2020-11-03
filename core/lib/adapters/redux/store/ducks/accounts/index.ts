585f46ef2174d229fdedb53375ddb1edc116e077
MrWilllian <willian.dev10@gmail.com>
2020-11-03 09:06:37 -0300

    refactor: include create account reducer

--- core/lib/adapters/redux/store/ducks/accounts/index.ts
+++ core/lib/adapters/redux/store/ducks/accounts/index.ts
@@ -10,13 +10,12 @@ const reducer: Reducer<AccountsState> = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case AccountsTypes.LOAD_SUCCESS:
       return { ...state, error: false, data: action.payload.data };
-    case AccountsTypes.LOAD_FAILURE: 
+    case AccountsTypes.LOAD_FAILURE:
       return { ...state, error: true, data: action.payload.data };
+    case AccountsTypes.CREATE_ACCOUNT:
+      return { ...state, error: false, data: [ ...state.data, action.payload.data ]};
     case AccountsTypes.DELETE_ACCOUNT:
-      return { 
-        ...state, error: false, 
-        data: state.data.filter((account: Account) => account.id !== action.payload.id )
-      };
+      return { ...state, error: false, accounts: state.data.filter((acc: Account) => acc.id !== action.payload.id) };
     default: 
       return state;
   }
