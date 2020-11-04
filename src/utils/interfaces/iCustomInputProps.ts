a3d9b233d778cefbceaeb440c7f1adeaf8160da7
MrWilllian <willian.dev10@gmail.com>
2020-11-04 11:19:37 -0300

    refactor: include containerStyle and style props

--- src/utils/interfaces/iCustomInputProps.ts
+++ src/utils/interfaces/iCustomInputProps.ts
@@ -5,6 +5,8 @@ export interface iCustomInputProps {
   name: string;
   value: string;
   icon?: string;
+  containerStyle?: {};
+  style?: {};
   onChangeText(operation?: any, rawValue?: any): any;
   onBlur?: (operation?: any) => any;
   showPassword?: boolean;
