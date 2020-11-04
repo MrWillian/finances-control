d1f88c078c67169588add9f7364c35070a3a232f
MrWilllian <willian.dev10@gmail.com>
2020-11-04 08:12:49 -0300

    refactor: include Formik and RN-Elements with validations

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,21 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { Alert } from 'react-native';
 import { useDispatch } from 'react-redux';
+import { Formik, FormikProps } from 'formik';
+import { Input } from "react-native-elements";
 
-import { CredentialsTypes } from '../../../core/lib/adapters/redux/store/ducks/credentials/types';
-import Input from '../../components/Input';
+// import Input from '../../components/Input';
 import { SimpleForm, Title, Button } from '../../components/FormBasicComponents';
 import { BottomInfo, BottomInfoText, BottomInfoLink } from '../../components/BottomInfoComponents';
 import { BackgroundGradient } from '../../components/Gradients';
 
+import { CredentialsTypes } from '../../../core/lib/adapters/redux/store/ducks/credentials/types';
 import { iNavigationProps, FieldType, CapitalizeType } from '../../utils';
 import { AuthController, StorageController } from '../../controllers';
 
 import { Container } from './styles';
+import { validationSchema } from './validationSchema';
+
+interface FormValues {
+  email: string;
+  password: string;
+}
 
 const Login: React.FC<iNavigationProps> = ({ navigation }) => {
-	const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+	// const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
   const dispatch = useDispatch();
@@ -32,10 +40,57 @@ const Login: React.FC<iNavigationProps> = ({ navigation }) => {
     getUserStorage();
   }, []);
 
-  async function handleLogin() {
+  const renderForm = ({ 
+    values, handleSubmit, setFieldValue, touched, errors, setFieldTouched, isSubmitting
+  }: FormikProps<FormValues>) => (
+    <SimpleForm>
+      <Input 
+        containerStyle={{minWidth: '90%',  marginTop: 10, elevation: 10 }}
+        inputContainerStyle={{ backgroundColor: '#FFF', paddingLeft: 10, borderRadius: 10 }}
+        inputStyle={{fontSize: 16}}
+        leftIcon={{ type: 'Ionicons', name: 'mail', color: '#2D142C', size: 25 }}
+        placeholder="Email"
+        value={values.email} 
+        // icon="mail" 
+        onChangeText={value => setFieldValue("email", value)}
+        onBlur={() => { { setFieldTouched('email')}}}
+        // focus={true}
+        // type={FieldType.TEXT}
+        textContentType='emailAddress'
+        keyboardType='email-address'
+        editable={!isSubmitting}
+        autoCapitalize={CapitalizeType.NONE}
+        autoCorrect={false}
+        autoCompleteType='email'
+        errorStyle={{ color: 'red' }}
+        errorMessage={touched.email && errors.email ? errors.email : undefined} />
+
+      <Input 
+        containerStyle={{minWidth: '90%', elevation: 10 }}
+        inputContainerStyle={{ backgroundColor: '#FFF', paddingLeft: 10, borderRadius: 10 }}
+        inputStyle={{fontSize: 16}}
+        leftIcon={{ type: 'Ionicons', name: 'lock', color: '#2D142C', size: 25 }}
+        placeholder="Senha"
+        secureTextEntry
+        autoCapitalize={CapitalizeType.NONE}
+        value={values.password}  
+        // icon="lock-closed" 
+        onChangeText={value => setFieldValue("password", value)}
+        onBlur={() => setFieldTouched('password')}
+        editable={!isSubmitting}
+        errorStyle={{ color: 'red' }}
+        errorMessage={touched.password && errors.password ? errors.password : undefined}
+        // type={FieldType.PASSWORD} 
+      />
+
+      <Button name="Entrar" isLoading={isLoading} onPress={handleSubmit} />
+    </SimpleForm>
+  );
+
+  async function handleLogin(values: FormValues) {
     setIsLoading(true);
 
-    await (authController.login(email, password)).then(user => {
+    await (authController.login(values.email, values.password)).then(user => {
       if (user.data == undefined) {
         Alert.alert('Erro', 'Ocorreu um erro ao tentar fazer login, tente novamente!', [
           { style: "cancel" }
@@ -54,29 +109,10 @@ const Login: React.FC<iNavigationProps> = ({ navigation }) => {
     <BackgroundGradient>
       <Container>
         <Title>Faça login com seu email e senha...</Title>
-        <SimpleForm>
-          <Input 
-            name="Email" 
-            value={email} 
-            icon="mail" 
-            onChangeText={(email: any) => setEmail(email)}
-            focus={true}
-            type={FieldType.TEXT}
-            textContentType='emailAddress'
-            keyboardType='email-address'
-            autoCapitalize={CapitalizeType.NONE}
-            autoCorrect={false}
-            autoCompleteType='email' />
-            
-          <Input 
-            name="Senha" 
-            value={password}  
-            icon="lock-closed" 
-            onChangeText={(password: any) => setPassword(password)}
-            type={FieldType.PASSWORD} />
 
-          <Button name="Entrar" isLoading={isLoading} onPress={handleLogin} />
-        </SimpleForm>
+        <Formik initialValues={{email: "", password: ""}} onSubmit={handleLogin} validationSchema={validationSchema}>
+          {(formikBag: FormikProps<FormValues>) => renderForm(formikBag)}
+        </Formik>
 
         <BottomInfo>
           <BottomInfoText>Não tem uma conta?</BottomInfoText>
