fb117a203386ef662e8c9c41f77c2b3189ced658
MrWilllian <willian.dev10@gmail.com>
2020-11-05 14:58:10 -0300

    refactor: change alert position

--- src/components/FlashMessage/styles.ts
+++ src/components/FlashMessage/styles.ts
@@ -1,16 +1,19 @@
+import { Dimensions } from 'react-native';
 import styled from 'styled-components/native';
+
+const height = Dimensions.get('window').height;
 
 export const Container = styled.View`
   position: absolute; 
-  background-color: green; 
-  width: 100%; 
+  background-color: green;
   justify-content: center; 
-  align-items: center;           
-  height: 40px;
-  bottom: -30px;
+  align-items: center;  
+  height: 50px;
+  width: 100%;
+  bottom: ${height - 405}px;
 `;
 
 export const Message = styled.Text`
   color: #FFF;
-  font-family: 'Comfortaa-Medium';
+  font-family: Comfortaa-Medium;
 `;
