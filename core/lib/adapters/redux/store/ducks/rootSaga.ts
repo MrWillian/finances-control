09b88a0384cb567525e30593f4ba7b14ec306025
MrWilllian <willian.dev10@gmail.com>
2020-11-13 09:11:51 -0300

    refactor: register transaction and category load saga methods

--- core/lib/adapters/redux/store/ducks/rootSaga.ts
+++ core/lib/adapters/redux/store/ducks/rootSaga.ts
@@ -2,11 +2,18 @@ import { all, takeLatest } from "redux-saga/effects";
 
 import { create, load, remove } from "./accounts/sagas";
 import { AccountsTypes } from "./accounts/types";
+import { TransactionsTypes, load as loadTransactions } from "./transactions";
+import { 
+  TransactionCategoriesTypes, load as loadTransactionCategories 
+} from "./transactionCategories";
 
 export default function* rootSaga() {
   return yield all([
     takeLatest(AccountsTypes.LOAD_REQUEST, load),
     takeLatest(AccountsTypes.CREATE_ACCOUNT, create),
     takeLatest(AccountsTypes.DELETE_ACCOUNT, remove),
+
+    takeLatest(TransactionsTypes.LOAD_REQUEST, loadTransactions),
+    takeLatest(TransactionCategoriesTypes.LOAD_REQUEST, loadTransactionCategories),
   ]);
 }
