d782fc63351b47a224e1907fcebba449a2a0dde1
MrWilllian <willian.dev10@gmail.com>
2020-11-13 09:14:15 -0300

    refactor: implement DateTimeInput on custom inputs

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-import { TextInput, PasswordInput, MoneyInput, PhoneInput } from '../CustomInputs';
+import { TextInput, PasswordInput, MoneyInput, PhoneInput, DateTimeInput } from '../CustomInputs';
 import { iCustomInputProps, FieldType } from '../../utils';
 
 import { Container } from './styles';
@@ -36,7 +36,12 @@ export const Input: React.FC<iCustomInputProps> = ({
         return (<MoneyInput 
           name={name} value={value} onChangeText={onChangeText} onBlur={onBlur} focus={focus} 
           includeRawValueInChangeText={includeRawValueInChangeText} />)
-          
+      
+      case FieldType.DATETIME:
+        return (<DateTimeInput 
+          name={name} value={value} onChangeText={onChangeText} onBlur={onBlur} focus={focus} 
+          includeRawValueInChangeText={includeRawValueInChangeText} />)
+
       default: 
         return;
     }
