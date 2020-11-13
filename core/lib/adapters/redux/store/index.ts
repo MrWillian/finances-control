1c76502e172242855ac38b6d00a61469eab4f935
MrWilllian <willian.dev10@gmail.com>
2020-11-13 09:14:56 -0300

    refactor: register transactions and categories on ApplicationState

--- core/lib/adapters/redux/store/index.ts
+++ core/lib/adapters/redux/store/index.ts
@@ -3,13 +3,17 @@ import createSagaMiddleware from 'redux-saga';
 
 import { AccountsState } from './ducks/accounts/types';
 import { CredentialsState } from './ducks/credentials/types';
+import { TransactionsState } from './ducks/transactions';
+import { TransactionCategoriesState } from './ducks/transactionCategories';
 
 import rootReducer from './ducks/rootReducer';
 import rootSaga from './ducks/rootSaga';
 
 export interface ApplicationState {
   accounts: AccountsState,
-  credentials: CredentialsState
+  credentials: CredentialsState,
+  transactions: TransactionsState,
+  transactionCategories: TransactionCategoriesState,
 }
 
 const sagaMiddleware = createSagaMiddleware();
