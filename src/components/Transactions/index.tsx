4a9a51a9ba40240af0643e91ce7176bd72a9555f
MrWilllian <willian.dev10@gmail.com>
2020-11-16 09:28:08 -0300

    refactor: include Transaction component

---
+++ src/components/Transactions/index.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import { Text } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigation } from '@react-navigation/native';
+import LinearGradient from 'react-native-linear-gradient';
+import ShimmerPlaceholder from 'react-native-shimmer-placeholder';
+
+import PlusButton from '../PlusButton';
+import TransactionCard from '../TransactionCard';
+import { Transaction } from '../../../core/lib/adapters/redux/store/ducks/transactions';
+import { ApplicationState } from '../../../core/lib/adapters/redux/store';
+import { loadRequest } from '../../../core/lib/adapters/redux/store/ducks/transactions';
+
+import { Container, Scroll, HeaderContainer, Title } from './styles';
+
+const Transactions: React.FC = () => {
+  const transactions = useSelector<ApplicationState, Transaction[]>(state => state.transactions.data);
+  const token = useSelector<ApplicationState, string>(state => state.credentials.token);
+  const [visible, setVisible] = useState(false);
+
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
+
+  const navigateToScreen = (screen: string) => navigation.navigate(screen);
+
+  useEffect(() => { dispatch(loadRequest(token)); }, []);
+
+  useEffect(() => { setTimeout(() => { setVisible(true) }, 2000); }, [transactions]);
+
+  return (
+    <Container>
+      <HeaderContainer>
+        <Title>Movimentações</Title>
+        <PlusButton onPress={() => navigateToScreen('NewTransaction')} />
+      </HeaderContainer>
+
+      <ShimmerPlaceholder 
+        style={{marginBottom: 5, width: '100%', borderRadius: 5}} LinearGradient={LinearGradient} visible={visible}>
+          
+        <Scroll horizontal={true}>
+          {transactions.length > 0 ?
+          transactions.map((transaction, index) => (
+            <TransactionCard 
+              key={index} 
+              title={transaction.description} 
+              value={transaction.value}
+              type={transaction.type} />
+          ))
+          :
+          <Text style={{fontFamily: 'Comfortaa-Medium', color: '#CCC'}}>Nenhuma movimentação criada ainda...</Text>
+          }
+        </Scroll>
+
+      </ShimmerPlaceholder>
+
+    </Container>
+  );
+}
+
+export default Transactions;
