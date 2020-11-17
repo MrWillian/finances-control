fb1e20360e0206faf5841b03b8d29172af5a3bd9
MrWilllian <willian.dev10@gmail.com>
2020-11-16 21:31:32 -0300

    refactor: include Balance values to enum

--- src/utils/enums/MenuBottomEnum.ts
+++ src/utils/enums/MenuBottomEnum.ts
@@ -1,5 +1,14 @@
 export enum MenuBottomEnum {
-  STATS = 'Stats', MAIN = 'Main', SETTINGS = 'Settings',
-  STATS_VALUE = 'Estatísticas', MAIN_VALUE = 'Início', SETTINGS_VALUE = 'Configurações',
-  STATS_ICON = "stats-chart", MAIN_ICON = "home", SETTINGS_ICON = "settings",
+  STATS = 'Stats', 
+  MAIN = 'Main', 
+  SETTINGS = 'Settings', 
+  BALANCE = 'Balance',
+  STATS_VALUE = 'Estatísticas', 
+  MAIN_VALUE = 'Início', 
+  SETTINGS_VALUE = 'Configurações',
+  BALANCE_VALUE = 'Balanço',
+  STATS_ICON = "stats-chart", 
+  MAIN_ICON = "home", 
+  SETTINGS_ICON = "settings",
+  BALANCE_ICON = "settings",
 }
