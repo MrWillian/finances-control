e2ecab5c4f9a3722226e0551dad1b6c2f53cd8f8
MrWilllian <willian.dev10@gmail.com>
2020-11-18 14:58:36 -0300

    style: include settings style

---
+++ src/screens/Settings/styles.ts
@@ -0,0 +1,46 @@
+import styled from 'styled-components/native';
+
+export const Content = styled.View`
+  padding: 30px;
+`;
+
+export const SettingsMenu = styled.View``;
+
+export const SettingsItemMenu = styled.View`
+  margin: 10px 0;
+`;
+
+export const Title = styled.Text`
+  font-family: Comfortaa-Bold;
+  font-size: 18px;
+  color: #C8C8C8;
+
+  margin-bottom: 5px;
+`;
+
+export const SettingsItem = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  margin: 10px 0;
+  border-bottom-width: 0.25px;
+  border-bottom-color: #C8C8C8;
+`;
+
+export const SettingsItemLabel = styled.Text`
+  font-family: Comfortaa-Regular;
+  font-size: 14px;
+  color: #97A6D7;
+`;
+
+export const SettingsItemValue = styled.TouchableOpacity`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const SettingsItemValueLabel = styled.Text`
+  color: #FFF;
+  margin-right: 5px;
+`;
