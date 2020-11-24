5e3cd13dcac2a0c1bf194b3e9dd78819f1da3051
MrWilllian <willian.dev10@gmail.com>
2020-11-24 14:46:29 -0300

    style: change on styles

--- src/screens/PrivacyPolicy/styles.ts
+++ src/screens/PrivacyPolicy/styles.ts
@@ -1,5 +1,34 @@
 import styled from 'styled-components/native';
 
 export const Content = styled.View`
+  height: 75%;
   padding: 0 30px;
+`;
+
+export const Scroll = styled.ScrollView``;
+
+export const Title = styled.Text`
+  font-family: Comfortaa-Bold;
+  font-size: 20px;
+  color: #FDFDFD;
+`;
+
+export const SubTitle = styled.Text`
+  font-family: Comfortaa-Bold;
+  font-size: 18px;
+  color: #FDFDFD;
+`;
+
+export const Paragraph = styled.Text`
+  font-family: Comfortaa-Regular;
+  color: #FDFDFD; 
+  margin-top: 10px;
+`;
+
+export const Bold = styled.Text`
+  font-family: Comfortaa-Bold;
+`;
+
+export const Underline = styled.Text`
+  text-decoration: underline;
 `;
