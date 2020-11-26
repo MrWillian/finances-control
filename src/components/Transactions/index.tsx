11983a4becdcdd1d6cc30050ef7311a36bae5fc6
MrWilllian <willian.dev10@gmail.com>
2020-11-26 15:01:07 -0300

    refactor: getting token from storage

--- src/components/Transactions/index.tsx
+++ src/components/Transactions/index.tsx
@@ -10,12 +10,16 @@ import TransactionCard from '../TransactionCard';
 import { Transaction } from '../../../core/lib/adapters/redux/store/ducks/transactions';
 import { ApplicationState } from '../../../core/lib/adapters/redux/store';
 import { loadRequest } from '../../../core/lib/adapters/redux/store/ducks/transactions';
+import { StorageController } from '../../controllers';
 
 import { Container, Scroll, HeaderContainer, Title } from './styles';
 
 const Transactions: React.FC = () => {
   const transactions = useSelector<ApplicationState, Transaction[]>(state => state.transactions.data);
   const token = useSelector<ApplicationState, string>(state => state.credentials.token);
+  let storageController = new StorageController();
+
+  const [tokenStorage, setTokenStorage] = useState('');
   const [visible, setVisible] = useState(false);
 
   const navigation = useNavigation();
@@ -23,9 +27,18 @@ const Transactions: React.FC = () => {
 
   const navigateToScreen = (screen: string) => navigation.navigate(screen);
 
-  useEffect(() => { dispatch(loadRequest(token)); }, []);
+  useEffect(() => { 
+    getTokenStorage();
+
+    if (token.length === 0) 
+      dispatch(loadRequest(tokenStorage));
+    else 
+      dispatch(loadRequest(token));
+  }, []);
 
   useEffect(() => { setTimeout(() => { setVisible(true) }, 2000); }, [transactions]);
+  
+  const getTokenStorage: any = async () => setTokenStorage((await storageController.getItem('@finances/user'))['access_token']);
 
   return (
     <Container>
